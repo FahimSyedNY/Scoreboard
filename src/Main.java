aa81363703d7f3ef39972e4880cf2c58de8bbba5
FahimSyed <fahims13@nycstundents.net>
2025-11-14 11:49:28 -0500

    Update with custom array methods.

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,35 @@ import java.awt.desktop.ScreenSleepEvent;
 import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Main {
+public abstract class Main {
     public static void main(String[] args) throws FileNotFoundException {
         File f = new File("Scoreboard.txt");
         Scanner s = new Scanner(f);
         String[] game = new String[] {};
-        int[] points = new int[0];
+        int[] points = new int[] {};
+        String[] teams = new String[] {};
+        boolean isTeam1 = true;
+        int index1 = 0;
+        int index2 = 0;
         while (s.hasNextLine()) {
             game = s.nextLine().split(" ");
-            points = new int[game.length - 2];
+            if (contains(teams, game[0])) index1 = index(teams, game[0]); else
+            if (contains(teams, game[1])) index1 = index(teams, game[1]);
             for (int i = 0; i < game.length - 2; i++) {
+                if (isTeam1)
                 points[i] = Integer.parseInt(game[i + 2]);
             }
         }
-        System.out.print(game[0]);
-        System.out.print(game[1]);
-        System.out.print(game[2]);
-        System.out.print(game[3]);
-        System.out.print(game[4]);
-        System.out.print(game[5]);
-        System.out.print(game[6]);
-        System.out.print(game[7]);
-        System.out.print(game[8]);
-        System.out.print(game[9]);
-        System.out.print(game[10]);
-        System.out.print(game[11]);
-        System.out.print(game[12]);
+    }
+    public static boolean contains(String[] x, String y) {
+        for (int i = 0; i < x.length; i++) if (y.equals(x[i])) return true;
+        return false;
+    }
+    public static int index(String[] x, String y) {
+        for (int i = 0; i < x.length; i++) if (y.equals(x[i])) return i;
+        return -1;
     }
 }
