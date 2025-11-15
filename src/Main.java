c42b008d60b0573667d6cfac6e338872381dce86
FahimSyedNY <fahims13@nycstudents.net>
2025-11-15 07:33:28 -0500

    Implement Map

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,19 @@
 import java.io.FileNotFoundException;
 import java.io.File;
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.*;
 
 public abstract class Main {
     public static void main(String[] args) throws FileNotFoundException {
         File f = new File("Scoreboard.txt");
         Scanner s = new Scanner(f);
         String[] game;
-        ArrayList<Integer> wins = new ArrayList<>();
-        ArrayList<String> teams = new ArrayList<>();
-        int[] index = new int[2];
+        Map<String, Integer> scores = new HashMap<String, Integer>();
         while (s.hasNextLine()) {
             boolean isTeam1 = true;
             int[] point = {0, 0};
             game = s.nextLine().split(" ");
-            for (int i = 0; i < 2; i++)
-                if (teams.contains(game[i])) index[i] = teams.indexOf(game[i]); else {
-                    teams.add(game[i]);
-                    index[i] = teams.indexOf(game[i]);
-                    wins.add(0);
-                }
+            scores.putIfAbsent(game[0], 0);
+            scores.putIfAbsent(game[1], 0);
             for (int i = 0; i < game.length - 2; i++)
                 if (isTeam1) {
                     point[0] += Integer.parseInt(game[i + 2]);
@@ -29,10 +22,9 @@ public abstract class Main {
                     point[1] += Integer.parseInt(game[i + 2]);
                     if ("0".equals(game[i + 2])) isTeam1 = true;
                 }
-            if (point[0] > point[1]) wins.set(index[0], wins.get(index[0]) + 1);
-            if (point[1] > point[0]) wins.set(index[1], wins.get(index[1]) + 1);
+            if (point[0] > point[1]) scores.put(game[0], scores.get(game[0]) + 1);
+            if (point[1] > point[0]) scores.put(game[1], scores.get(game[1]) + 1);
         }
-        System.out.println(teams);
-        System.out.println(wins);
+        System.out.println(scores);
     }
 }
